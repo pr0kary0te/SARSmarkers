3491504e85c4c760b309d3db1de28b6e7519f968
pr0kary0te <62709458+pr0kary0te@users.noreply.github.com>
2021-04-01 14:53:47 +0100

    Update allele_fre_by_date.pl

--- allele_fre_by_date.pl
+++ allele_fre_by_date.pl
@@ -4,7 +4,6 @@
 `mkdir week_analysis`;
 
 
-
 #primer_designs_for_cog.tdt
 $markers = $ARGV[0];
 chomp $markers;
@@ -16,8 +15,8 @@ while(<IN>)
 {
 chomp;
 ($marker, $seq) = split(/\t/, $_);
-if($marker =~/CoV_(\d+)/){$base = $1;}
-if($seq =~ /\[([A-Z])\/([A-Z])\]/){$lookup{$base}{$1}++; $lookup{$base}{$2}++; print "$base $1 $2\n";}
+if($marker =~/CoV_(\d+)/){$pos = $1;}
+if($seq =~ /\[([A-Z])\/([A-Z])\]/){$lookup{$pos}{$1}++; $lookup{$pos}{$2}++; print "$pos $1 $2\n";}
 }
 
 
@@ -36,6 +35,7 @@ while(<META>)
 $name2week{$name} = $week;
 }
 close META;
+print "Loaded metadata\n";
 
 $seq = $ARGV[2];
 open(SEQ, "$seq");
@@ -50,12 +50,17 @@ foreach $pos(sort keys %lookup)
 {
 $actual = $pos -1;
 $match = substr($line,$actual,1);
-if($lookup{$pos}{$match} >0){$final{$pos}{$week}{$match}++; $weeks{$week}++; $overall_total{$pos}{$match}++;}
+#Make sure this is a valid SNP allele:
+if($lookup{$pos}{$match} >0){$final{$pos}{$week}{$match}++; $weeks{$week}++; $overall_total{$pos}{$match}++; $genotypes{$name}.=$match;}
+
+
 }
 
 }
 }
+$n = keys %genotypes;
 
+print "Loaded Sequence data for $n samples\n";
 
 
 foreach $pos(keys %final)
@@ -96,4 +101,64 @@ foreach $pos(sort {$a<=>$b} keys %grouped)
 print OUT "$pos $major{$pos}";
 foreach $week (sort {$a<=>$b} keys %weeks){print OUT "\t$grouped{$pos}{$week}";}
 print OUT "\n";
+}
+
+
+
+$n = keys %lookup;
+
+foreach $name(keys %genotypes)
+{
+$week = $name2week{$name};
+$genotype = $genotypes{$name};
+$l = length($genotype);
+if($n == $l)
+ {
+ $complete_genotypes{$week}{$genotype}++;
+ }
+}
+
+print "
+To test the discriminatory power of the selected markers over time, the genotypes observed in each week are placed in random order\n
+and then adjacent genotypes compared to see if they are differnt. For example, if there are 50 samples sequenced in week 5, and the\n
+pipeline produced 24 markers, we find the 24-marker haplotypes present in this week and put them in random order. We then compare the\n
+1st versus 2nd, 2nd verus 3rd, 3rd versus 4th haplotype and so on until we reach the 49th versus 50th. There will be n-1 comparisons\n\n\n";  
+print "\nWeek\tDifferences\tComparisons\tPercent comparisons different\n";
+foreach $week(sort {$a<=>$b} keys %complete_genotypes)
+{
+$ref = $complete_genotypes{$week};
+%hash = %$ref;
+@complete_genotypes = ();
+$n = keys %hash;
+foreach $genotype(keys %hash)
+  {
+  $cases = $hash{$genotype};
+  while($cases >0) { push @complete_genotypes, $genotype; $cases--;}
+  }
+
+shuffle (\@complete_genotypes);
+$l = @complete_genotypes;
+$i = 0;
+$j = 1;
+$diff = 0;
+$comparisons = $l -1;
+ while ($j < $l)
+ {
+ if($complete_genotypes[$i] ne $complete_genotypes[$j]){$diff++;}
+ $i++; $j++;
+ }
+if($week =~ /\d/ && $comparisons >0){$prop = int(100*($diff/$comparisons)); print "$week\t$diff\t$comparisons\t$prop\n";}
+}
+
+
+
+sub shuffle
+{
+my $array = shift;
+my $i;
+for ($i = @$array; --$i;) {
+my $j = int(rand($i+1));
+next if $i == $j;
+@$array[$i,$j] = @$array[$j,$i];
+}
 }
